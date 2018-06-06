ab7a61fbbc0f8a9535e29fe69ba1e79a976ca323
HeroOfChaos <schwarzer.s93@t-online.de>
2018-06-06 17:26:41 +0200

    Listen in Netzwerkform für die ganze Familie

--- extrahiercode.py
+++ extrahiercode.py
@@ -5,8 +5,12 @@ Created on Sat May 19 13:01:58 2018
 @author: BlackEmperor
 """
 
+#Außer "re" müssen alle Module auf dem Server Terminal mit "pip install MODUL --user" installiert werden (z.B. pip3 install rdflib --user)
 import rdflib
+import networkx as nx
 import re
+import pandas as pd
+import matplotlib.pyplot as plt
 
 dbtropes_rdf = rdflib.Graph()
 dbtropes_rdf.parse("/home/heckelenme/tvtropes_projektseminar/dbtropes_snapshot.nt", format = "nt")
@@ -75,32 +79,17 @@ regex = re.compile(r'/Film/')
 #In die Variable "filter_werkliste" wird eine Liste geschrieben, in der die Filme mithilfe der
 #Regular Expression aus der ursprünglichen Werkliste durch den regex.search Befehl gefiltert wurden.
 filter_werkliste = list(filter(regex.search, werkliste))
-
+#Dann erstellen wir eine Typliste, wo der Knotentyp (in diesem Fall "work") für jedes Werk abgelegt
+#wird. Die werden später für die Attribute des jeweiligen Knoten gebraucht.
+for filterwerk in filter_werkliste:
+    typeliste.extend([{"type": "work"}])
+    
 #Test zum Anzeigen der gefilterten Werkliste.
 print(filter_werkliste)
 
 #Jetzt haben wir zumindest mal eine Liste von Filmen aus dem "Rated M for Manly" Genre.
 
-#3. Query: Comedy Tropes
-#Der Query holt alle Tropes aus dem Datensatz, die über "processingCategory2" mit der ComedyTropes
-#Ressource verbunden sind (sprich, alle Comedy Tropes).
-query_output = dbtropes_rdf.query(
-        """
-        SELECT ?trope
-        WHERE {
-            ?trope <http://dbtropes.org/ont/processingCategory2> <http://dbtropes.org/resource/Main/ComedyTropes>
-        }
-        """
-    )
-
-#Ausführung und Füllen der Liste.
-for row in query_output:
-    comedytrope_liste.extend(row)
-
-#Test zum Anzeigen der Comedy Trope Liste.
-print(comedytrope_liste)
-
-#4. Query: Tropes der Filme
+#3. Query: Tropes der Filme
 #Der Query läuft per for-Schleife über die gefilterte Werkliste.
 #Hier wird wieder eine Variable "trope" definiert. Hier wird die "Auflösung" der komplizierten
 #Feature-Verbindung komplett in den Query gepackt. Heißt:
@@ -120,9 +109,6 @@ for filterwerk in filter_werkliste:
     )
 #Zuerst packen wir die Strings der gefilterten Werke (ohne diesen rdf URI Rotz vor der URI) in eine Liste.
     knotenliste.extend([str(filterwerk)])
-#Dann erstellen wir eine Typliste, wo der Knotentyp (in diesem Fall "work") für jedes Werk abgelegt
-#wird. Die werden später für die Attribute des jeweiligen Knoten gebraucht.
-    typeliste.extend({"type": "work"})
 #Hier wird der Query dann erst wirklich ausgeführt. Für jedes gefundene Trope wird ein Tuple Eintrag
 #in der bipartiten Kantenliste angelegt, dann werden die einzelnen Tropes der Knotenliste hinzugefügt
 #und zu guter Letzt wird die Typliste mit dem Knotentyp "Trope" für jedes Trope erweitert.
@@ -135,3 +121,55 @@ for filterwerk in filter_werkliste:
 print(knotenliste)
 print(typeliste)
 print(bipartite_kantenliste)
+
+##### TROPE LISTEN ERSTELLEN #####
+#4. Query: Comedy Tropes
+#Der Query holt alle Tropes aus dem Datensatz, die über "processingCategory2" mit der ComedyTropes
+#Ressource verbunden sind (sprich, alle Comedy Tropes).
+query_output = dbtropes_rdf.query(
+        """
+        SELECT ?trope
+        WHERE {
+            ?trope <http://dbtropes.org/ont/processingCategory2> <http://dbtropes.org/resource/Main/ComedyTropes>
+        }
+        """
+    )
+
+#Ausführung und Füllen der Liste.
+for row in query_output:
+    comedytrope_liste.extend(row)
+
+#Test zum Anzeigen der Comedy Trope Liste.
+print(comedytrope_liste)
+
+##### LISTEN IN NETZWERKFORM BRINGEN #####
+##### ACHTUNG! WORK IN PROGRESS #####
+#Typliste und Knotenliste werden zusammengeführt. Jedes Element in der Liste wird nun zu
+#einem Tupel, (Werk/Trope, type: work/trope).
+knoten = list(zip(knotenliste, typeliste))
+
+#Hier wird ein leerer Graph angelegt, in den erst die Knoten aus der zusammengelegten
+#Knotenliste (Variable "knoten") und dann die Kanten aus der bipartiten Kantenliste eingefügt werden
+trope_network = nx.Graph()
+trope_network.add_nodes_from(knoten)
+trope_network.add_edges_from(bipartite_kantenliste)
+
+#Hier wird irgendwas namens "bipartite" importiert. Braucht man wohl für den nachfolgenden Code.
+from networkx.algorithms import bipartite
+
+#Hier wird der Inhalt aus der zusammengeführten Knotenliste in Werkknoten und Tropeknoten
+#aufgespalten. Dafür haben wir zuvor die Typliste gebraucht.
+trope_nodes = [k for k, v in dict(knoten).items() if v["type"] ==  "trope" ]
+work_nodes = [k for k, v in dict(knoten).items() if v["type"] == "work" ]
+
+#???
+trope_network_tropes_only = bipartite.projected_graph(trope_network, trope_nodes, multigraph=False)
+
+#Datei wird in Gephi-Format exportiert; Usernamen im Pfad durch euren ersetzen
+nx.write_gexf(trope_network, "/home/schwarzersn/trope_network.gexf")
+
+#Diese Datei sollte man per scp (pscp) Befehl vom Server runterladen können. Den genauen
+#Befehl weiß ich noch nicht.
+
+#Nutzlosen roten Farbkleks malen. Dauert lange.
+nx.draw(trope_network)
